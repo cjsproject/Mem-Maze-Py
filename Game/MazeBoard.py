e8c0d96cb037e04a5eef3cb6c4392d19aef68302
cucumberhands <cyrus.simons@yahoo.com>
2020-07-28 11:25:24 -0400

    maze points fully generated

--- Game/MazeBoard.py
+++ Game/MazeBoard.py
@@ -35,24 +35,24 @@ class BoardGame(Board):
 
         while y < self._Board__dim-1:
             random.seed(time.time())
-            direction = random.randint(0, 4)
+            direction = random.randint(0, 3)
             print(direction)
             current = self.__mazePoints[i]
-            if direction == 0 and y+1 < self._Board__dim and self.__mazePoints[i-1] != (current[0], current[1]+1): #up
+            if direction == 0 and y+1 < self._Board__dim and (current[0], current[1]+1) not in self.__mazePoints: #up
                 self.__mazePoints.append(
                     (current[0], current[1]+1)
                 )
                 y += 1
                 i += 1
                 continue
-            elif direction == 1 and y != 0 and self.__mazePoints[i-1][1] != current[1]-1: #down, y is not 0, and is not equal to the point before
+            elif direction == 1 and y != 0 and (current[0], current[1]-1) not in self.__mazePoints: #down, y is not 0, and is not equal to the point before
                 self.__mazePoints.append(
                     (current[0], current[1]-1)
                 )
                 y -= 1
                 i += 1
                 continue
-            elif direction == 2 and xp != 0 and self.__mazePoints[i-1] != (current[0]-1, current[1]):
+            elif direction == 2 and xp != 0 and (current[0]-1, current[1]) not in self.__mazePoints:
                 #randomDirection == Direction.LEFT && prevX - 1 >= 0 && board[y][prevX - 1].equals("-")
                 #left, current x is not next x
                 self.__mazePoints.append(
@@ -61,7 +61,7 @@ class BoardGame(Board):
                 xp -= 1
                 i += 1
                 continue
-            elif direction == 3 and xp+1 < self._Board__dim:
+            elif direction == 3 and xp+1 < self._Board__dim and (current[0]+1, current[1]) not in self.__mazePoints:
                 self.__mazePoints.append(
                     (current[0]+1, current[1])
                 )
@@ -72,16 +72,21 @@ class BoardGame(Board):
                 continue
 
     def __str__(self):
-        return str(self.__mazePoints)
+        for i in self._Board__board:
+            for j in i:
+                if j in self.__mazePoints:
+                    print("(", self.__mazePoints.index(j) + 1, ")", end='\t')
+                else:
+                    print(j, end='\t')
+            print()
+        return "Maze Points: " + str(self.__mazePoints)
 
 
 x = BoardGame()
 
 #print("\n",x._BoardGame__mazePoints[0], x._BoardGame__mazePoints[0][0]+1, sep='\t')
 print(x)
-z = [[ (x,y) for x in range(3)] for y in range(3)]
 
-print(z[0][0])
 
 
 
